a503c1f51bf9ed4128ad597e79a379ea2557d2c7
Laraib Memon <laraibmemon.bssef22@iba-suk.edu.pk>
2025-11-12 20:57:28 +0500

    update in attributes of students

--- src/student/Students.java
+++ src/student/Students.java
@@ -6,15 +6,15 @@ public class Students {
     private String password;
     private String department;
     private String guardianName;
-    private String contact;
+    private String stcontact;
 
-    public Students(String cmsId, String name, String password, String department, String guardianName, String contact) {
+    public Students(String cmsId, String name, String password, String department, String guardianName, String stcontact) {
         this.cmsId = cmsId;
         this.name = name;
         this.password = password;
         this.department = department;
         this.guardianName = guardianName;
-        this.contact = contact;
+        this.stcontact = stcontact;
     }
 
     // Getters
@@ -23,10 +23,10 @@ public class Students {
     public String getPassword() { return password; }
     public String getDepartment() { return department; }
     public String getGuardianName() { return guardianName; }
-    public String getContact() { return contact; }
+    public String getContact() { return stcontact; }
 
     // Convert to line for file saving
     public String toFileString() {
-        return cmsId + "," + name + "," + password + "," + department + "," + guardianName + "," + contact;
+        return cmsId + "," + name + "," + password + "," + department + "," + guardianName + "," + stcontact;
     }
 }
